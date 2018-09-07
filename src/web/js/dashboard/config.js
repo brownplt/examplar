eb59d73fdea965d425c48dbabffb5e1231e6172f
Jack Wrenn <me@jswrenn.com>
2018-09-07 15:40:21 -0400

    change page titles to Examplar
    fixes #1, continued

--- src/web/js/dashboard/config.js
+++ src/web/js/dashboard/config.js
@@ -1,5 +1,5 @@
 // App name in *Title Case*.
-export const APP_NAME = 'code.pyret.org';
+export const APP_NAME = 'Examplar';
 
 // File extension of code files for this app.
 export const FILE_EXT = 'arr';
